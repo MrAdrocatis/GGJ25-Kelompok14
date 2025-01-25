a05447417720e42c7ec00dd0d5731b08a51266c6
StallPlayz <stallplayz123@gmail.com>
2025-01-26 02:14:24 +0700

    Adding some animation, and some asset to it

---
+++ GGJ_2025_Project/Assets/Dev-Yusuf/Script/Animation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+internal class AnimationStrings
+{
+    internal static string IsMoving = "IsMoving";
+    internal static string IsShooting = "IsShooting";
+    internal static string shoot = "shoot";
+}
--- GGJ_2025_Project/Assets/Dev-Yusuf/Script/Movement.cs
+++ GGJ_2025_Project/Assets/Dev-Yusuf/Script/Movement.cs
@@ -1,29 +1,104 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Movement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
 {
-    [Header("movement")]
-    public float movementspeed = 12f;
-    private float horizontalMovement;
-    private Rigidbody2D rB;
+    public float walkSpeed = 12f;
+    Vector2 moveInput;
 
-    //[Header("jumping")]
-    //public float jumpforce = 20f;
-    //private bool justJumped = false;
+    [SerializeField]
+    private bool _isMoving = false;
 
-    [Header("ground")]
-    public bool onground = false;
-
-    private void Start() => rB = GetComponent<Rigidbody2D>();
-
-    private void Update()
+    public bool IsMoving
     {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        get
+        {
+            return _isMoving;
+
+        }
+        private set
+        {
+
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.IsMoving, value);
+
+        }
     }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                // flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+
+        }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     private void FixedUpdate()
     {
-        rB.velocity = new Vector2(x: horizontalMovement * movementspeed, rB.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    public void onMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // face the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger(AnimationStrings.shoot);
+        }
     }
 }
-
-    
