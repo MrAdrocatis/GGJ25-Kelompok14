9e6db9c5404c228bf1dbfa3c3489e1e94705eb14
Adrocatis <mr.adrocatis@gmail.com>
2025-01-25 14:27:26 +0700

    Playable test game
    coba aja dulu gan

--- GGJ_2025_Project/Assets/Dev-Yusuf/Script/EnemySpawner.cs
+++ GGJ_2025_Project/Assets/Dev-Yusuf/Script/EnemySpawner.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomEnemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     [System.Serializable]
     public class EnemyPrefab
